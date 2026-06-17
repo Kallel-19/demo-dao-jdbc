e01893757329a9f079c2bddefce1b1f615d0afa7
kallel <kallel-10@hotmail.com>
2026-06-17 10:31:04 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,6 +11,8 @@ public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
